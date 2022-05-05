2d6b39fcd937c0dc509bbfd70f785c27fee88c9c
Elliiee <danyiluozhang@gmail.com>
2022-05-05 15:54:53 -0500

    bfs

--- Q1971.java
+++ Q1971.java
@@ -35,4 +35,39 @@ public class Q1971 {
 
         return false;
     }
+
+    public boolean validPathBFS(int n, int[][] edges, int start, int end){
+        List<List<Integer>> adjacencyList = new ArrayList<>();
+
+        for (int i = 0; i < n; i++){
+            adjacencyList.add(new ArrayList<>());
+        }
+
+        for (int[] edge : edges){
+            adjacencyList.get(edge[0]).add(edge[1]);
+            adjacencyList.get(edge[1]).add(edge[0]);
+        }
+
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(start);
+
+        boolean seen[] = new boolean[n];
+        Arrays.fill(seen, false);
+        seen[start] = true;
+
+        while (!queue.isEmpty()){
+            int node = queue.poll();
+
+            if (node == end)
+                return true;
+
+            for (int neighbor : adjacencyList.get(node)){
+                if (!seen[neighbor]){
+                    seen[neighbor] = true;
+                    queue.add(neighbor);
+                }
+            }
+        }
+        return false;
+    }
 }
