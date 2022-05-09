16622f5ec9635bf8a984980debb4657b7b5d1d60
Elliiee <danyiluozhang@gmail.com>
2022-05-09 17:02:38 -0500

    moving average

---
+++ Q364.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Q364 {
+    private int size;
+    List<Integer> queue = new ArrayList<>();
+
+    public Q364(int size) {
+        this.size = size;
+    }
+
+    public double next(int val) {
+        queue.add(val);
+        int windowSum = 0;
+
+        for (int i = Math.max(0, queue.size() - size); i < queue.size(); i++){
+            windowSum += (int)queue.get(i);
+        }
+
+        return windowSum * 1.0 / Math.min(queue.size(), size);
+    }
+}
