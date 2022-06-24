32e0b99fc67170bc36f837dbfe013b36688fa88b
Elliiee <danyiluozhang@gmail.com>
2022-06-24 16:19:47 -0500

    062422

---
+++ Q35.java
@@ -0,0 +1,19 @@
+public class Q35 {
+    // search insert position
+    public int searchInsert(int[] nums, int target) {
+        int mid, left = 0, right = nums.length - 1;
+        while (left <= right){
+            mid = left + (right - left) / 2;
+            if (nums[mid] == target){
+                return mid;
+            }
+
+            if (target < nums[mid]){
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+}
