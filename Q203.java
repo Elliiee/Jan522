3c3360c5ff8a46cdb3289ca7663e852ceabfa66a
Elliiee <danyiluozhang@gmail.com>
2022-07-25 14:03:22 -0500

    203

---
+++ Q203.java
@@ -0,0 +1,17 @@
+public class Q203 {
+    // remove linked list elements
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode prev = dummy, current = head;
+        while (current != null){
+            if (current.val == val){
+                prev.next = current.next;
+            } else {
+                prev = current;
+            }
+            current = current.next;
+        }
+        return dummy.next;
+    }
+}
