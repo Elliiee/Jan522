61873fa5edc1021fac90517fcd5df81c4d227acd
Elliiee <danyiluozhang@gmail.com>
2022-07-27 14:25:09 -0500

    669

---
+++ Q669.java
@@ -0,0 +1,16 @@
+public class Q669 {
+    // trim a binary search tree
+    public TreeNode trimBST(TreeNode root, int low, int high) {
+        if (root == null) return null;
+
+        if (root.val > high)
+            return trimBST(root.left, low, high);
+        if (root.val < low)
+            return trimBST(root.right, low, high);
+
+        root.left = trimBST(root.left, low, high);
+        root.right = trimBST(root.right, low, high);
+
+        return root;
+    }
+}
