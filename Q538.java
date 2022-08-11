9b86d1e7af5fb7b873b3738655f17e95b1ac0259
Elliiee <danyiluozhang@gmail.com>
2022-08-11 13:15:52 -0500

    538

---
+++ Q538.java
@@ -0,0 +1,18 @@
+public class Q538 {
+    //convert bst to greater tree
+    private int sum = 0;
+
+    public TreeNode convertBST(TreeNode root) {
+        traverse(root);
+        return root;
+    }
+
+    private void traverse(TreeNode node){
+        if (node == null) return;
+
+        traverse(node.right);
+        sum += node.val;
+        node.val = sum;
+        traverse(node.left);
+    }
+}
